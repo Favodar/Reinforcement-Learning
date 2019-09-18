5b8b15448814e44e2d1a5a5b065404d2266afe30
Andreas Plewnia <acplewnia@gmail.com>
2019-09-18 14:40:28 +0200

    Init canvas

--- RLwebsite/js/quiz.js
+++ RLwebsite/js/quiz.js
@@ -1,139 +1,82 @@
-const circleDiameter = 200;
-const squareDiameter = 200;
-const tries = 3;
-const numParticles = 50;
+var canvas = document.querySelector('canvas');
+var innerWidth = window.innerWidth;
+var innerHeight = window.innerHeight;
 
-var circleX;
-var circleY;
-var squareX;
-var squareY;
+var attempts = 3;
+var successes = 0;
 
-var clickTime;
-var currentTime;
-var currentTry;
-var reward;
-var penalty;
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
-var particles;
+var ctx = canvas.getContext('2d');
 
-function setup() {
-    createCanvas(windowWidth, windowHeight);
-    background(66, 66, 200);
+function Circle() {
+    this.radius = 30;
+    this.x = Math.random() * (innerWidth - this.radius * 2) + this.radius;
+    this.y = Math.random() * (innerHeight - this.radius * 2) + this.radius;
+    this.dx = (Math.random() - 0.5) * 2;
+    this.dy = (Math.random() - 0.5) * 2;
 
-    randomPos();
-    currentTry = 0;
-    reward = false;
-    penalty = false;
-
-    particles = [];
-    for (var i = 0; i < numParticles; i++) {
-        particles.push(new Particle(-50, -50));
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = 'grey';
+        ctx.fill();
     }
-}
 
-function windowResized() {
-    resizeCanvas(windowWidth, windowHeight);
-}
-
-
-//'game' loop
-function draw() {
-    currentTime = millis();
-    if (currentTry < tries) {
-        if (reward) {
-            if (currentTime - clickTime > 1000) {
-                randomPos();
-                currentTry++;
-                reward = false;
-            }
-            background(66, 200, 66);
+    this.update = function () {
+        if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+            this.dx = -this.dx;
         }
-        else if (penalty) {
-            if (currentTime - clickTime > 1000) {
-                randomPos();
-                penalty = false;
-            }
-            background(200, 66, 66);
+        if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+            this.dy = -this.dy;
         }
-        else {
-            background(66, 66, 200);
+
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+}
+
+function Square() {
+    this.width = 60;
+    this.height = 60;
+    this.x = Math.random() * (innerWidth - this.width);
+    this.y = Math.random() * (innerHeight - this.height);
+    this.dx = (Math.random() - 0.5) * 2;
+    this.dy = (Math.random() - 0.5) * 2;
+
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'grey';
+        ctx.fill();
+    }
+
+    this.update = function () {
+        if (this.x + this.width > innerWidth || this.x < 0) {
+            this.dx = -this.dx;
         }
-        rect(squareX, squareY, squareDiameter, squareDiameter);
-        ellipse(circleX, circleY, circleDiameter, circleDiameter);
-        for (var i = 0; i < numParticles; i++) {
-            particles[i].x += particles[i].dx;
-            particles[i].y += particles[i].dy;
-            noStroke();
-            fill(particles[i].color);
-            ellipse(particles[i].x, particles[i].y, 5, 5);
+        if (this.y + this.height > innerHeight || this.y < 0) {
+            this.dy = -this.dy;
         }
-        stroke(0, 0, 0, 255);
-        fill(255, 255, 255);
-    }
-    else {
-        background(200, 200, 200);
-        //resizeCanvas(0, 0);
-        //setActiveStyleSheet("parallax")
-        //window.location.replace("parallax.html");
-        window.location.assign("parallax.html");
-        
+
+        this.x += this.dx;
+        this.y += this.dy;
     }
 }
 
-function mousePressed() {
-    clickTime = millis();
-    if (mouseOverSquare()) {
-        penalty = true;
-    }
-    else if (mouseOverCircle()) {
-        for (var i = 0; i < numParticles; i++) {
-            particles[i].x = mouseX;
-            particles[i].y = mouseY;
-        }
-        reward = true;
-    }
+var circle = new Circle();
+var square = new Square();
+
+function animate() {
+    requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, innerWidth, innerHeight);
+
+    circle.draw();
+    square.draw();
+
+    circle.update();
+    square.update();
 }
 
-function randomPos() {
-    circleX = random(windowWidth - circleDiameter);
-    circleY = random(windowHeight - circleDiameter);
-    squareX = random(windowWidth - squareDiameter);
-    squareY = random(windowHeight - squareDiameter);
-}
-
-function mouseOverSquare() {
-    return mouseX > squareX
-        && mouseX < squareX + squareDiameter
-        && mouseY > squareY
-        && mouseY < squareY + squareDiameter;
-}
-
-function mouseOverCircle() {
-    return mouseX > circleX - circleDiameter / 2
-        && mouseX < circleX + circleDiameter / 2
-        && mouseY > circleY - circleDiameter / 2
-        && mouseY < circleY + circleDiameter / 2;
-}
-
-class Particle {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        let radians = random(3.14159 * 2);
-        let speed = random(1, 10);
-        this.dx = Math.cos(radians) * speed;
-        this.dy = Math.sin(radians) * speed;
-        this.color = color(random(255), random(255), random(255), 255);
-    }
-}
-
-/**function setActiveStyleSheet(title) {
-   var i, a, main;
-   for(i=0; (a = document.getElementsByTagName("link")<i>); i++) {
-     if(a.getAttribute("rel").indexOf("style") != -1
-        && a.getAttribute("title")) {
-       a.disabled = true;
-       if(a.getAttribute("title") == title) a.disabled = false;
-     }
-   }
-}**/
+animate();
